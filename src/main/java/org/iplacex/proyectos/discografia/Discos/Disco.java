05b79f31d0ea728526d00802e6e9d13cd82edfed
Jose Serra <148002646+jofran93@users.noreply.github.com>
2025-09-18 23:16:14 -0300

    Update Disco.java

--- src/main/java/org/iplacex/proyectos/discografia/Discos/Disco.java
+++ src/main/java/org/iplacex/proyectos/discografia/Discos/Disco.java
@@ -13,4 +13,40 @@ public class Disco {
     public String nombre;
     public int anioLanzamiento;
     public List<String> canciones;
+    public String getIdArtista() {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'getIdArtista'");
+    }
+    public Object getNombre() {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'getNombre'");
+    }
+    public void setNombre(Object nombre2) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'setNombre'");
+    }
+    public Object getAnioLanzamiento() {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'getAnioLanzamiento'");
+    }
+    public void setAnioLanzamiento(Object anioLanzamiento2) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'setAnioLanzamiento'");
+    }
+    public Object getCanciones() {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'getCanciones'");
+    }
+    public void setCanciones(Object canciones2) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'setCanciones'");
+    }
+    public void setIdArtista(String idArtista2) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'setIdArtista'");
+    }
+    public void setId(Object object) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'setId'");
+    }
 }
